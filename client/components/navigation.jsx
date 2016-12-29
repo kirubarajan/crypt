6375ed43e9a50078fb96b7540cb8beff8d296672
Arun Kirubarajan <arun@Aruns-MacBook-Pro.local>
2016-12-29 12:46:32 -0500

    added github auth to nav

--- client/components/navigation.jsx
+++ client/components/navigation.jsx
@@ -1,22 +1,72 @@
 import React, { Component } from 'react';
-import { Navbar, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
+import { Navbar, NavbarBrand, Nav, NavItem, NavLink, Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
+
+import AccountsUIWrapper from './accounts';
 
 class Navigation extends Component {
+
+  constructor(props) {
+
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+
+    this.state = {
+
+      dropdownOpen: false
+
+    };
+
+  }
+
+  toggle() {
+
+    this.setState({
+
+      dropdownOpen: !this.state.dropdownOpen
+
+    });
+
+  }
 
   render() {
 
     return (
 
       <Navbar color="faded" light>
+
         <NavbarBrand href="/">elore</NavbarBrand>
-        <Nav className="float-xs-right" navbar>
-        <NavItem>
-          <NavLink href="">About</NavLink>
-        </NavItem>
+
+        <Nav className="float-xs-left" navbar>
+
           <NavItem>
-            <NavLink href="">Sign In</NavLink>
+
+            <NavLink href="/gary"> Gary </NavLink>
+
           </NavItem>
+
+          <NavItem>
+
+            <Dropdown isOpen={this.state.dropdownOpen} toggle={this.toggle}>
+
+              <DropdownToggle caret>
+
+                Account
+
+              </DropdownToggle>
+
+              <DropdownMenu>
+
+                <DropdownItem header> <AccountsUIWrapper /> </DropdownItem>
+
+              </DropdownMenu>
+
+            </Dropdown>
+
+          </NavItem>
+
         </Nav>
+
       </Navbar>
 
     );

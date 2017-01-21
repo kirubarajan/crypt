78a96f4615f703b00cce2f873755f5cf9f1e9287
jeffzhangg <zhang.jeffreyd@gmail.com>
2017-01-21 00:17:36 -0500

    Add Logic to Core Container and Site Creator Component

--- client/containers/CoreContainer.jsx
+++ client/containers/CoreContainer.jsx
@@ -4,12 +4,29 @@ import SiteCreator from '../components/SiteCreator';
 import SiteList from '../components/SiteList'
 
 class CoreContainer extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      sites: []
+    };
+  }
+
+  // update state - specifically array of cards
+  handleCreate(website, url) {
+    console.log('works');
+    console.log(website, url);
+    //send backend this info
+      const object = Meteor.call('accounts.insert', website, url);
+      console.log(object);
+  }
+
   render() {
     return (
       <div className="container-fluid">
         <div className="row">
           <div className="col col-sm-6">
-            <SiteCreator />
+            <SiteCreator
+              onCreate={this.handleCreate.bind(this)}/>
           </div>
 
           <div className="col col-sm-6">

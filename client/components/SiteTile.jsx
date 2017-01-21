171c2461b26ca82056e1332a404401ca603d2609
Jevin Sidhu <jevin.sidhu@gmail.com>
2017-01-21 09:05:08 -0500

    Add Copy To Clipboard Functionality

--- client/components/SiteTile.jsx
+++ client/components/SiteTile.jsx
@@ -1,5 +1,35 @@
 import React from 'react';
 
+import ReactDOM from 'react-dom';
+import CopyToClipboard from 'react-copy-to-clipboard';
+
+const App = React.createClass({
+  getInitialState() {
+    return {value: '', copied: false};
+  },
+
+  render() {
+    return (
+      <div>
+        <input className="copy-input" value={this.state.value}
+          onChange={({target: {value}}) => this.setState({value, copied: false})} />&nbsp;
+
+        <CopyToClipboard text={this.state.value}
+          onCopy={() => this.setState({copied: true})}>
+          <button className="copy-button">Copy to clipboard</button>
+        </CopyToClipboard>&nbsp;
+
+        {this.state.copied ? <span>Copied</span> : null}
+      </div>
+    );
+  }
+});
+
+const appRoot = document.createElement('div');
+document.body.appendChild(appRoot);
+ReactDOM.render(<App />, appRoot);
+
+
 class SiteTile extends React.Component {
   render() {
     return (

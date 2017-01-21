c53cabdc6eab5bec750fff398c13dbc5b879861b
Jevin Sidhu <jevin.sidhu@gmail.com>
2017-01-21 02:21:56 -0500

    Adjust Input Box Styling

---
+++ client/components/splash.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Splash extends React.Component {
+
+  render() {
+    return (
+      <div>
+        <h1> Hello, World </h1>
+        <p> another test </p>
+      </div>
+    );
+
+  }
+
+}
+
+export default Splash;

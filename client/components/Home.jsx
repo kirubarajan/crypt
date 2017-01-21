9f75f7434841e3a4f2536ab6b8479b5773cb56de
jeffzhangg <zhang.jeffreyd@gmail.com>
2017-01-20 21:46:15 -0500

    Fix Home and Signup

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -5,22 +5,26 @@ import { Link } from 'react-router';
 class Home extends React.Component {
   render() {
     return (
-      <div>
-        <h1> Home </h1>
-        <Form>
-         <FormGroup>
-           <Label for="exampleEmail">Email</Label>
-           <Input type="email" name="email" id="exampleEmail" placeholder="email" />
-         </FormGroup>
-         <FormGroup>
-           <Label for="examplePassword">Password</Label>
-           <Input type="password" name="password" id="examplePassword" placeholder="password" />
-         </FormGroup>
-         <Button>Enter</Button>
-         <Link to='/signup'>
-           <Button>Signup</Button>
-         </Link>
-       </Form>
+      <div className="container">
+        <div className="row align-items-sm-center justify-content-sm-center">
+          <div className="col col-sm-12">
+            <h1>Home</h1>
+            <Form>
+             <FormGroup>
+               <Label for="exampleEmail">Email</Label>
+               <Input type="email" name="email" id="exampleEmail" placeholder="email" />
+             </FormGroup>
+             <FormGroup>
+               <Label for="examplePassword">Password</Label>
+               <Input type="password" name="password" id="examplePassword" placeholder="password" />
+             </FormGroup>
+             <Button>Enter</Button>
+             <Link to='/signup'>
+               <Button>Signup</Button>
+             </Link>
+           </Form>
+          </div>
+        </div>
      </div>
     )
   }
--- client/components/Signup.jsx
+++ client/components/Signup.jsx
@@ -4,24 +4,28 @@ import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
 class Signup extends React.Component {
   render() {
     return (
-      <div>
-        <h1>Signup</h1>
-        <Form>
-          <FormGroup>
-            <Label for="exampleEmail">Email</Label>
-            <Input type="email" name="email" id="exampleEmail" placeholder="email" />
-          </FormGroup>
-          <FormGroup>
-            <Label for="examplePassword">Password</Label>
-            <Input type="password" name="password" id="examplePassword" placeholder="password" />
-          </FormGroup>
-          <FormGroup>
-            <Label for="examplePassword">Repeat Password</Label>
-            <Input type="password" name="password" id="examplePassword" placeholder="repeat password" />
-          </FormGroup>
-          <Button>Enter</Button>
-        </Form>
-      </div>
+      <div className="container">
+        <div className="row align-items-sm-center justify-content-sm-center">
+          <div className="col col-sm-12">
+            <h1>Signup</h1>
+            <Form>
+              <FormGroup>
+                <Label for="exampleEmail">Email</Label>
+                <Input type="email" name="email" id="exampleEmail" placeholder="email" />
+              </FormGroup>
+              <FormGroup>
+                <Label for="examplePassword">Password</Label>
+                <Input type="password" name="password" id="examplePassword" placeholder="password" />
+              </FormGroup>
+              <FormGroup>
+                <Label for="examplePassword">Repeat Password</Label>
+                <Input type="password" name="password" id="examplePassword" placeholder="repeat password" />
+              </FormGroup>
+              <Button>Enter</Button>
+            </Form>
+          </div>
+        </div>
+     </div>
     );
   }
 }

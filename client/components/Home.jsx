6a5a2eab43194bca075404b7be1e6899dd1db6c3
Jevin Sidhu <jevin.sidhu@gmail.com>
2017-01-21 01:35:28 -0500

    Polish Sign In + Sign Up

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -5,24 +5,24 @@ import { Link } from 'react-router';
 class Home extends React.Component {
   render() {
     return (
-      <div className="container">
+      <div className="container one-edge-shadow">
         <div className="row align-items-sm-center justify-content-sm-center">
-          <div className="col col-sm-12">
-            <h1>Home</h1>
+          <div className="col-left col col-sm-6">
+            <h1>SIGN IN</h1>
             <Form>
              <FormGroup>
-               <Label for="exampleEmail">Email</Label>
-               <Input type="email" name="email" id="exampleEmail" placeholder="email" />
+               <Input type="email" name="email" id="exampleEmail" placeholder="Email" />
              </FormGroup>
              <FormGroup>
-               <Label for="examplePassword">Password</Label>
-               <Input type="password" name="password" id="examplePassword" placeholder="password" />
+               <Input type="password" name="password" id="examplePassword" placeholder="Password" />
              </FormGroup>
-             <Button>Enter</Button>
              <Link to='/signup'>
-               <Button>Signup</Button>
-             </Link>
+             <Button className="register">REGISTER</Button>
+           </Link>
+               <Button className="signin">SIGN IN</Button>
            </Form>
+          </div>
+          <div className="col-right col col-sm-6">
           </div>
         </div>
      </div>

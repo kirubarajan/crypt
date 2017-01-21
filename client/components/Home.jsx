65fa97da36f94abbe08ef6de7b60f7ca20d140ba
Jevin Sidhu <jevin.sidhu@gmail.com>
2017-01-20 20:54:09 -0500

    Add Home Page

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
+import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
 
 class Home extends React.Component {
   render() {
     return (
       <div>
-        <h1>Home</h1>
-      </div>
+        <h1> Home </h1>
+        <Form>
+         <FormGroup>
+           <Label for="exampleEmail">Email</Label>
+           <Input type="email" name="email" id="exampleEmail" placeholder="email" />
+         </FormGroup>
+         <FormGroup>
+           <Label for="examplePassword">Password</Label>
+           <Input type="password" name="password" id="examplePassword" placeholder="password" />
+         </FormGroup>
+         <Button>Submit</Button>
+       </Form>
+     </div>
     )
   }
 }

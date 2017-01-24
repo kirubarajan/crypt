15db506436a79f8037afda9e5033f8634e4d98cd
jeffzhangg <zhang.jeffreyd@gmail.com>
2017-01-24 11:17:52 -0500

    Refactor landing page

--- client/components/Landing.jsx
+++ client/components/Landing.jsx
@@ -16,17 +16,18 @@ class Landing extends React.Component {
         {/* </Link> */}
       </div>
 
-      <div className="landing-info col col-xs-12">
-        <img className="landing-info-img center-block" src="images/lower-logo.png" />
-        <p className="landing-paragraph landing-info-paragraph"> Crypt generates email addresses & passwords for  your online accounts.
+      <div className="landing__info col col-xs-12">
+        <img src="images/lower-logo.png" />
+        <p> Crypt generates email addresses & passwords for  your online accounts.
           <br/><br/>
-          Using these credentials means that when a platform you use is <a className="hack" href="https://www.nytimes.com/2016/12/14/technology/yahoo-hack.html" target="_blank"> hacked </a> -- and user information is exposed -- your personal email won't be compromised and the password is disposable.
+          Using these credentials means that when a platform you use is <a href="https://www.nytimes.com/2016/12/14/technology/yahoo-hack.html" target="_blank"> hacked </a> -- and user information is exposed -- your personal email won't be compromised and the password is disposable.
           <br/><br/>
-          Account information on Crypt is encrypted using <a href="http://csrc.nist.gov/publications/fips/fips197/fips-197.pdf" target="_blank"> <span className="highlight">AES256</span></a> (hacker mans ain't  shit).</p>
+          Account information on Crypt is encrypted using <a href="http://csrc.nist.gov/publications/fips/fips197/fips-197.pdf" target="_blank">AES256</a> (hacker mans ain't  shit).
+        </p>
       </div>
 
-      <div className="landing-footer col col-xs-12">
-        <p className="landing-paragraph landing-footer-paragraph"> made by arun, brian, jeff and jevin </p>
+      <div className="landing__footer col col-xs-12">
+        <p className="landing-paragraph landing-footer-paragraph"> made by <a href="https://github.com/a19n" target="_blank">arun</a>, brian, <a href="https://github.com/jeffzhangg" target="_blank">jeff</a> and <a href="https://github.com/JevinSidhu" target="_blank">jevin</a></p>
       </div>
 
     </div>

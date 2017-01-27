8b25254bf51b7348697c39f7ac6299c74cad4c1c
jeffzhangg <zhang.jeffreyd@gmail.com>
2017-01-27 14:05:53 -0500

    Refactor login page

--- client/components/Login.jsx
+++ client/components/Login.jsx
@@ -25,27 +25,23 @@ class Login extends React.Component {
 
   render() {
     return (
-      <div className=" logincontainer one-edge-shadow">
-        <div className="row">
+      <div className="login one-edge-shadow">
+        <div className="login__wrapper row">
 
-          <div className="col col-md-6 col-sm-12">
+          <div className="login__form col col-md-6 col-sm-12">
             <h1>Login</h1>
-            <Form onSubmit={this.onLogin.bind(this)} className="home-form">
-             <FormGroup>
-               <input type="email" name="email" id="exampleEmail" placeholder="Email" ref="email"/>
-             </FormGroup>
-             <FormGroup>
-               <input type="password" name="password" id="examplePassword" placeholder="Password" ref="password"/>
-             </FormGroup>
-
-             <Button className="home-button signin"><p>Enter</p></Button>
-             <Link to='/signup'>
-              <Button className="home-button register"><p>Reigster</p></Button>
-            </Link>
-           </Form>
+            <form onSubmit={this.onLogin.bind(this)} className="home-form">
+              <input type="email" id="exampleEmail" placeholder="Email" ref="email"/>
+              <input type="password" id="examplePassword" placeholder="Password" ref="password"/>
+              <br />
+              <button className="home-button signin"><p>Sign in</p></button>
+              <Link to='/signup'>
+                <p className="login__form__small-paragraph">Create account</p>
+              </Link>
+           </form>
           </div>
 
-          <div className="col col-md-6 col-xs-12">
+          <div className="login__splash col col-md-6 col-xs-12">
           </div>
         </div>
      </div>
